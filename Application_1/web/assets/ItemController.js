e216f08a528ede170967b9f406b7ab5ca93a6d4f
vishmee <Vishanchathuranga2003@gmail.com>
2024-12-31 03:15:12 +0530

    item

--- Application_1/web/assets/ItemController.js
+++ Application_1/web/assets/ItemController.js
@@ -1,13 +1,47 @@
 let select_item_row = null;
 
 /*===========================================================================================================================*/
+const generateNextIdItem = (items) => {
+    let maxId = 0;
+    items.forEach(item => {
+        const numericPart = parseInt(item.item_id.substring(1)); // Extract numeric part
+        if (numericPart > maxId) {
+            maxId = numericPart;
+        }
+    });
+
+    const nextNumericId = maxId + 1;
+    return `I${nextNumericId.toString().padStart(3, '0')}`;
+};
+
+const autoGenerateItemNextId = () => {
+    $.ajax({
+        url: "http://localhost:8080/Application_1_war_exploded/item",
+        type: "GET",
+        success: (response) => {
+            console.log("Fetched Item:", response);
+
+            let nextId = "I001";
+            if (response.length > 0) {
+                nextId = generateNextIdItem(response);
+            }
+
+            $('#item_id').val(nextId);
+        },
+        error: (err) => {
+            console.error("Error fetching item:", err);
+        }
+    });
+};
+
+/*===========================================================================================================================*/
 $(document).ready(function (){
     autoGenerateItemNextId();
     loadItemTable();
     loadAllItem();
 })
 
-/*===========================================================================================================================*/
+/*==================================================== Load All Item=======================================================================*/
 const loadAllItem = () => {
     $.ajax({
         url: "http://localhost:8080/Application_1_war_exploded/item",
